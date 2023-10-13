8598bde0af899de776c507516d9d344663451fd0
Robin Chien <goodnana1224@gmail.com>
2023-10-14 01:05:21 +0800

    Add springboot application

--- src/main/java/com/robinchien/GithubCicdActionsApplication.java
+++ src/main/java/com/robinchien/GithubCicdActionsApplication.java
@@ -1,7 +1,12 @@
 package com.robinchien;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
 
     @GetMapping("/welcome")
@@ -10,6 +15,7 @@ public class GithubCicdActionsApplication {
     }
 
     public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
         System.out.println("Hello world!");
     }
 }
